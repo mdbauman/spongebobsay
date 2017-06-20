b3c76d9f9bcac47155e433b7d29aa40df8433395
Matt Bauman <mdbauman@gmail.com>
2017-06-19 23:56:52 -0400

    Args, piping, fix wrapping for figlet etc

--- spongebobsay.py
+++ spongebobsay.py
@@ -9,9 +9,11 @@ import os
 import random
 import shutil
 import textwrap
+import argparse
 
 #force utf-8 writer in case terminal is a silly encoding
 sys.stdout = codecs.getwriter('utf8')(sys.stdout.buffer)
+SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
 
 def derpify_text(words):
     ret = []
@@ -20,11 +22,12 @@ def derpify_text(words):
         ret.append(uc)
     return ret
 
-def wrap_text(words):
-    terminal_size = shutil.get_terminal_size(fallback=(80, 20))
-    width = 40 if terminal_size.columns>40 else terminal_size.columns
+def wrap_text(words, width):
+    if not width:
+        terminal_size = shutil.get_terminal_size(fallback=(80, 20))
+        width = 40 if terminal_size.columns>40 else terminal_size.columns
 
-    return textwrap.wrap(' '.join(words),width)
+    return textwrap.wrap(' '.join(words),int(width))
 
 def make_balloon(lines):
     max_len = max(len(line) for line in lines)
@@ -42,28 +45,53 @@ def make_balloon(lines):
         else:
             balloon = balloon+'| '+line.ljust(max_len,' ')+' |\n'
     
-    balloon = balloon + last_line+'\n  \\\n   \\\n    \\\n'
+    balloon = balloon + last_line+'\n  \\\n   \\\n'
     return balloon
 
-def get_bob():
+def random_bob():
     files = []
-    for fname in os.listdir('bobs'):
+    for fname in os.listdir(SCRIPT_DIR+'/bobs'):
         files.append(fname)
     return random.choice(files)
 
 def print_bob(fname):
-    with codecs.open('bobs/'+fname, 'r', encoding='unicode_escape') as file:
+    with codecs.open(SCRIPT_DIR+'/bobs/'+fname, 'r', encoding='unicode_escape') as file:
         lines = file.readlines()
         for line in lines:
             print(line, end='')
 
 def main():
-    words = derpify_text(sys.argv[1:])
-    lines = wrap_text(words)
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-n', '--nowrap', help="Disable automatic line wrap", action='store_true')
+    parser.add_argument('-d', '--derpify', help="Derpify text", action='store_true')
+    parser.add_argument('-w', '--width', help="Set max width, ignored if using -n", action='store')
+    parser.add_argument('-f', '--file', help="Specify .bob file, ex. 'mocking.bob'", action='store')
+    opts, text = parser.parse_known_args()
+
+    #cesi n'est pas une pipe (use pipe if no text)
+    if(len(text)==0):
+        for line in sys.stdin:
+            text.append(line)
+    
+    if opts.derpify:
+        text = derpify_text(text)
+    if opts.nowrap:
+        lines = ''.join(text).rstrip().split('\n')
+    else:
+        lines = wrap_text(text, opts.width)
+    if opts.file:
+        bobfile = SCRIPT_DIR+'/bobs/'+opts.f
+    else:
+        bobfile = random_bob()
+
+    if(lines==str(lines)):
+        lines = [lines,]
+
     balloon = make_balloon(lines)
     print(balloon)
-    #print(lines)
-    #print_bob(get_bob())
+    print_bob(bobfile)
+    print('\033[m') #default color
 
 if __name__ == '__main__':
     main()
